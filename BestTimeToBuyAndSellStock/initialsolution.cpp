b7a35798bce62165786d6d071e273b4ab274745a
Ali Mirzakhalili <alimknu@gmail.com>
2025-02-22 15:30:27 -0500

    Adding BestTimeToBuyAndSellStock

---
+++ BestTimeToBuyAndSellStock/initialsolution.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int highProfit(vector<int> &prices)
+    {
+        int low = 0;
+        int high = 0;
+        int profit = 0;
+
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (prices[i] < prices[low])
+            {
+                low = i;
+                high = i;
+            }
+            else if (prices[i] >= prices[high])
+            {
+                high = i;
+            }
+
+            if (prices[high] > prices[low])
+            {
+                profit = max(profit, (prices[high] - prices[low]));
+            }
+        }
+
+        return profit;
+    }
+};
